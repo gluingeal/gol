73086ca3493b725732142737ad5f80fd454ce8bc
Pavel Martínek <gluingeal@gmail.com>
2021-05-31 23:35:38 +0200

    lekce2

--- code.js
+++ code.js
@@ -1,33 +1,76 @@
 var rows = 24;
 var cols = 24;
 
+var playing = false;
+
 // initialize
 function initialize() {
-    createTable();
+  createTable();
+  setupControlButtons();
+}
+
+function cellClickHandler() {
+  var classes = this.getAttribute('class');
+  if (classes.indexOf('live') > -1) {
+    this.setAttribute('class', 'dead');
+  } else {
+    this.setAttribute('class', 'live');
+  }
 }
 
 // lay out the board
 function createTable() {
-    var gridContainer = document.getElementById("gridContainer");
-    if (!gridContainer) {
-        // throw error
-        console.error("Problem: no div for the grid table!");
-    }
-    var table = document.createElement("table");
+  var gridContainer = document.getElementById('gridContainer');
+  if (!gridContainer) {
+    // throw error
+    console.error('Problem: no div for the grid table!');
+  }
+  var table = document.createElement('table');
 
-    for (var i = 0; i < rows; i++) {
-        var tr = document.createElement("tr");
-        for (var j = 0; j < cols; j++) {
-            var cell = document.createElement("td");
-            cell.setAttribute("id", i + "_" + j);
-            cell.setAttribute("class", "dead");
-            tr.appendChild(cell);
-        }
-        table.appendChild(tr);
+  for (var i = 0; i < rows; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 0; j < cols; j++) {
+      var cell = document.createElement('td');
+      cell.setAttribute('id', i + '_' + j);
+      cell.setAttribute('class', 'dead');
+      cell.onclick = cellClickHandler;
+      tr.appendChild(cell);
     }
-    gridContainer.appendChild(table);
+    table.appendChild(tr);
+  }
+  gridContainer.appendChild(table);
 }
 
+function setupControlButtons() {
+  var startButton = document.getElementById('start');
+  startButton.onclick = startButtonHandler;
+  var clearButton = document.getElementById('clear');
+  clearButton.onclick = clearButtonHandler;
+}
+
+function clearButtonHandler() {
+    playing = false;
+    var startButton = document.getElementById('start');
+    startButton.innerHTML = "start"
+
+}
+
+function startButtonHandler() {
+  if (playing) {
+    console.log('Pause the Game');
+    playing = false;
+    this.innerHTML = 'continue';
+  } else {
+    console.log('Cont the game');
+    playing = true;
+    this.innerHTML = 'pause';
+    play();
+  }
+}
+
+function play() {
+    console.log("Play the game")
+}
 
 // start everything
 window.onload = initialize;
