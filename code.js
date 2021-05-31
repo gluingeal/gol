f94b45e1d043a516099207d556af43cc70748942
Pavel Martínek <gluingeal@gmail.com>
2021-06-01 00:20:27 +0200

    lekce4

--- code.js
+++ code.js
@@ -23,9 +23,9 @@ function resetGrids() {
 }
 
 function cellClickHandler() {
-    var rowcol = this.id.split("_");
-    var row = rowcol[0];
-    var col = rowcol[1];
+  var rowcol = this.id.split('_');
+  var row = rowcol[0];
+  var col = rowcol[1];
   var classes = this.getAttribute('class');
   if (classes.indexOf('live') > -1) {
     this.setAttribute('class', 'dead');
@@ -87,6 +87,61 @@ function startButtonHandler() {
 
 function play() {
   console.log('Play the game');
+  computeNextGen();
+}
+
+function computeNextGen() {
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      applyRules(i, j);
+    }
+  }
+}
+
+function applyRules(row, col) {
+  var numNeighbors = countNeighbors(row, col);
+  if (grid[row][col] == 1) {
+    if (numNeighbors < 2) {
+      nextGrid[row][col] = 0;
+    } else if (numNeighbors == 2 || numNeighbors == 3) {
+      nextGrid[row][col] = 1;
+    } else if (numNeighbors > 3) {
+      nextGrid[row][col] = 0;
+    }
+  } else if (grid[row][col] == 0) {
+    if (numNeighbors == 3) {
+      nextGrid[row][col] = 1;
+    }
+  }
+}
+
+function countNeighbors(row, col) {
+  var count = 0;
+  if (row - 1 >= 0) {
+    if (grid[row - 1][col] == 1) count++;
+  }
+  if (row - 1 >= 0 && col - 1 >= 0) {
+    if (grid[row - 1][col - 1] == 1) count++;
+  }
+  if (row - 1 >= 0 && col + 1 < cols) {
+    if (grid[row - 1][col + 1] == 1) count++;
+  }
+  if (col - 1 >= 0) {
+    if (grid[row][col - 1] == 1) count++;
+  }
+  if (col + 1 < cols) {
+    if (grid[row][col + 1] == 1) count++;
+  }
+  if (row + 1 < rows) {
+    if (grid[row + 1][col] == 1) count++;
+  }
+  if (row + 1 < rows && col - 1 >= 0) {
+    if (grid[row + 1][col - 1] == 1) count++;
+  }
+  if (row + 1 < rows && col + 1 < cols) {
+    if (grid[row + 1][col + 1] == 1) count++;
+  }
+  return count;
 }
 
 // initialize
